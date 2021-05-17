6be6365a31a8e86c7277c68f4181f9c764eb0c58
arvinmoj <arvinmojaverian@hotmail.com>
2021-05-17 16:17:37 +0430

    first commit

---
+++ Data/Migrations/20210213075738_MyFirstMigration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestModels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestModels", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestModels");
+        }
+    }
+}
---
+++ My_Application/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Data;
+using System.Linq;
+using ViewModels.Error;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace My_Application.Controllers
+{
+    public class HomeController : Infrastructure.BaseControllerWithDatabase
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public HomeController(DatabaseContext databaseContext): base(databaseContext)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
